1716c1498f19146302177f4c289a0b4db0051cf1
Zhiqian Qiao <zhiqianq@andrew.cmu.edu>
2017-08-03 11:27:47 -0400

    Finish sim

--- project_new/inter_sim.py
+++ project_new/inter_sim.py
@@ -19,8 +19,8 @@ class InterSim(object):
     Inter_Left = - 4.
     Inter_Right = 4.
     Vehicle_NO = 3
-    Visual = True
     Cft_Accel = 3.     # m/s**2
+    Visual = False
 
     def __init__(self):
         self.sim = None
@@ -32,10 +32,10 @@ class InterSim(object):
         self.hv_poses = []
         for i in range(self.Vehicle_NO):
             hv_pos = dict()
-            hv_pos['y'] = self.av_pos['y'] + random.random() * 100.
+            hv_pos['y'] = self.av_pos['y'] + random.random() * 100. + 20.
             hv_pos['x'] = 2.
             hv_pos['vx'] = 0.
-            hv_pos['vy'] = random.random() * self.Speed_limit
+            hv_pos['vy'] = self.Speed_limit - random.random()
             self.hv_poses.append(hv_pos)
         self.target_dis = None
         self.target_v = None
@@ -44,37 +44,38 @@ class InterSim(object):
 
     def draw_scenary(self, av, hvs):
         if self.Visual:
-            fig = plt.figure(1)
-            ax = fig.add_axes()
+            plt.figure(1)
             plt.plot(0, self.Inter_Ori, 'g.', markersize=10)
             plt.plot(list(range(4)), list([self.Stop_Line] * 4), 'g')
-            plt.plot(list(xrange(-50, 50)), list([self.Inter_Low] * 100), 'r')
-            plt.plot(list(xrange(-50, 50)), list([self.Inter_Up] * 100), 'r')
+            plt.plot(list(xrange(-20, 20)), list([self.Inter_Low] * 40), 'r')
+            plt.plot(list(xrange(-20, 20)), list([self.Inter_Up] * 40), 'r')
             plt.plot(list([self.Inter_Left] * 400), list(xrange(-200, 200)), 'r')
             plt.plot(list([self.Inter_Right] * 400), list(xrange(-200, 200)), 'r')
             plt.plot(self.Inter_Right / 2., self.Pass_Point, 'g.', markersize=10)
             plt.plot(av['x'], av['y'], 'r.', markersize=15)
-            fig.text(av['x'], av['y'], 'aaaa', fontsize=20, color='black')
+            plt.text(av['x'], av['y'], str(av['vy']))
             for hv in hvs:
                 plt.plot(hv['x'], hv['y'], 'c.', markersize=15)
-                fig.text(hv['x'], hv['y'], str(hv['vy']), fontsize=20, color='black')
+                plt.text(hv['x'], hv['y'], str(hv['vy']))
             plt.show()
             plt.pause(0.2)
             plt.clf()
 
     def get_state(self, a=0):
         self.update_vehicle(a)
-        front_dis = self.hv_poses['y'] - self.av_pos['y']
+        front_dis = [hv_pos['y'] - self.av_pos['y'] for hv_pos in self.hv_poses]
         sl_dis = self.Stop_Line - self.av_pos['y']
-        dis_pool = [front_dis, sl_dis]
+        dis_pool = [sl_dis] + front_dis
         self.target_dis = min(dis_pool)
         self.target_v = 0. if dis_pool.index(min(dis_pool)) == 0 else self.hv_poses['vy']
         self.state = np.array([self.av_pos['vy'], self.target_v, self.target_dis], ndim=2)
         self.state_dim = np.shape(self.state)[1]
+        print 'Accel: ', a, ', V_av = ', self.av_pos['vy'], ', Distance Pool: ', dis_pool
 
     def update_vehicle(self, a=0):
         for hv_pos in self.hv_poses:
-            hv_a = - 0.5 * (hv_pos['vy'] ** 2) / hv_pos['y'] if hv_pos['y'] <= self.Stop_Line else self.Cft_Accel
+            hv_a = - 0.5 * (hv_pos['vy'] ** 2) / (self.Stop_Line - hv_pos['y']) if hv_pos['y'] < self.Stop_Line - 1 \
+                else self.Cft_Accel
             hv_pos['vy'] += hv_a * self.Tau
             hv_pos['vy'] = min(max(0.1, hv_pos['vy']), self.Speed_limit)
             hv_pos['y'] += hv_pos['vy'] * self.Tau + 0.5 * hv_a * (self.Tau ** 2)
@@ -90,3 +91,4 @@ if __name__ == '__main__':
     plt.ion()
     while True:
         sim.update_vehicle()
+        sim.get_state()
